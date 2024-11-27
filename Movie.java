85574a25589e4b7a93a8c4d3377d084454a6d831
Ajay <145901281+Ajay-g25@users.noreply.github.com>
2024-11-27 14:06:21 +0530

    Create Movie.java

---
+++ Movie.java
@@ -0,0 +1,18 @@
+public class Movie {
+    private String title;
+    private int duration; // in minutes
+ 
+    public Movie(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+ 
+    public String getTitle() {
+        return title;
+    }
+ 
+    public int getDuration() {
+        return duration;
+    }
+}
+ 
